92fbb9ea41b7097af0cb1ad0de66d9913e5e1dc0
Jesse Madrigales <madrigje@oregonstate.edu>
2020-10-21 17:57:14 -0700

    Implemented basic login page

---
+++ lib/login_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Pager"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              Text(
+                'Login Information',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                  onSaved: (value) => _email = value,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: "Email Address")),
+              TextFormField(
+                  onSaved: (value) => _password = value,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: "Password")),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                  child: Text("LOGIN"),
+                  onPressed: () {
+                    final form = _formKey.currentState;
+                    form.save();
+
+                    // Validate will return true if is valid, or false if invalid.
+                    if (form.validate()) {
+                      print("$_email $_password");
+                    }
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
