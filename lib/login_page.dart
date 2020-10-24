9c9be35c90bce9c253f715ad5728370b10723dc2
Jesse Madrigales <madrigje@oregonstate.edu>
2020-10-23 18:58:50 -0700

    Completed simple login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:disc/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   static const routeName = 'loginpage';
@@ -23,11 +24,7 @@ class _LoginPageState extends State<LoginPage> {
           key: _formKey,
           child: Column(
             children: <Widget>[
-              SizedBox(height: 20.0),
-              Text(
-                'Login Information',
-                style: TextStyle(fontSize: 20),
-              ),
+              _logo(context),
               SizedBox(height: 20.0),
               // TextFormField(
               //     onSaved: (value) => _email = value,
@@ -40,6 +37,8 @@ class _LoginPageState extends State<LoginPage> {
               _emailPasswordWidget(_email, _password),
               SizedBox(height: 20.0),
               _submitButton(context),
+              SizedBox(height: 20.0),
+              _continue(context),
               // RaisedButton(
               //     child: Text("LOGIN"),
               //     onPressed: () {
@@ -60,63 +59,95 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 Widget _entryField(String title, String word, {bool isPassword = false}) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextFormField(
-              obscureText: isPassword,
-              onSaved: (value) => word = value,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  fillColor: Color(0xfff3f3f4),
-                  filled: true))
-        ],
-      ),
-    );
-  }
-
-  Widget _emailPasswordWidget(String _email, String _password) {
-    return Column(
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        _entryField("Email id", _email),
-        _entryField("Password", _password, isPassword: true),
+        Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+            obscureText: isPassword,
+            onSaved: (value) => word = value,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                fillColor: Color(0xfff3f3f4),
+                filled: true))
       ],
-    );
-  }
+    ),
+  );
+}
+
+Widget _emailPasswordWidget(String _email, String _password) {
+  return Column(
+    children: <Widget>[
+      _entryField("Username", _email),
+      _entryField("Password", _password, isPassword: true),
+    ],
+  );
+}
 
 Widget _submitButton(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.symmetric(vertical: 15),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.grey.shade200,
-                offset: Offset(2, 4),
-                blurRadius: 5,
-                spreadRadius: 2)
-          ],
-          gradient: LinearGradient(
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    padding: EdgeInsets.symmetric(vertical: 15),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.grey.shade200,
+              offset: Offset(2, 4),
+              blurRadius: 5,
+              spreadRadius: 2)
+        ],
+        gradient: LinearGradient(
             colors: [Color(0xff2193b0), Color(0xff6dd5ed)],
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight
-              )
-              ),
-      child: Text(
-        'Login',
-        style: TextStyle(fontSize: 20, color: Colors.white),
-      ),
-    );
-  }
-  
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight)),
+    child: Text(
+      'Login',
+      style: TextStyle(fontSize: 20, color: Colors.white),
+    ),
+  );
+}
+
+Widget _continue(BuildContext context) {
+  return GestureDetector(
+      onTap: () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+          (Route<dynamic> route) => false,
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            alignment: Alignment.center,
+            child: Text('Forgot Password?',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            alignment: Alignment.center,
+            child: Text('Continue without logging in?',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+          ),
+        ],
+      ));
+}
+
+Widget _logo(BuildContext context) {
+  return Container (
+    child: Image.asset(
+      'assets/images/flutter.png',
+      width: 100.0),
+  );
+}
