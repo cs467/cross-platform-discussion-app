05f678b2a6726cdc6e263ad407ac6c00db5fae5f
Jesse Madrigales <madrigje@oregonstate.edu>
2020-11-06 16:04:26 -0600

    Added suffixIcon to TextFormFields

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -13,14 +13,44 @@ class SignUpPage extends StatefulWidget {
 
 class _SignUpPageState extends State<SignUpPage> {
   final _formKey = GlobalKey<FormState>();
+  bool submit;
 
   TextEditingController emailController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
   TextEditingController usernameController = new TextEditingController();
+  TextEditingController confirmPasswordController = new TextEditingController();
+
+ @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    usernameController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    emailController.addListener(() {
+      setState(() {});
+    });
+    passwordController.addListener(() {
+      setState(() {});
+    });
+    usernameController.addListener(() {
+      setState(() {});
+    });
+    confirmPasswordController.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: true,
         resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           title: Text("Sign Up Page"),
@@ -64,6 +94,7 @@ class _SignUpPageState extends State<SignUpPage> {
         _usernameField("Username"),
         _emailField("Email"),
         _passwordField("Password", isPassword: true),
+        //_confirmPasswordField("Confirm Password", isPassword: true)
       ],
     );
   }
@@ -87,6 +118,11 @@ class _SignUpPageState extends State<SignUpPage> {
               obscureText: isPassword,
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
+                 suffixIcon: usernameController.text.length > 0
+                      ? IconButton(
+                          onPressed: () => usernameController.clear(),
+                          icon: Icon(Icons.cancel, color: Colors.grey))
+                      : null,
                   border: InputBorder.none,
                   hintText: 'Enter Username',
                   fillColor: Color(0xfff3f3f4),
@@ -115,6 +151,11 @@ class _SignUpPageState extends State<SignUpPage> {
               obscureText: isPassword,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
+                 suffixIcon: emailController.text.length > 0
+                      ? IconButton(
+                          onPressed: () => emailController.clear(),
+                          icon: Icon(Icons.cancel, color: Colors.grey))
+                      : null,
                   border: InputBorder.none,
                   hintText: 'Enter Email',
                   fillColor: Color(0xfff3f3f4),
@@ -142,6 +183,11 @@ class _SignUpPageState extends State<SignUpPage> {
               obscureText: isPassword,
               keyboardType: TextInputType.name,
               decoration: InputDecoration(
+                 suffixIcon: passwordController.text.length > 0
+                      ? IconButton(
+                          onPressed: () => passwordController.clear(),
+                          icon: Icon(Icons.cancel, color: Colors.grey))
+                      : null,
                   border: InputBorder.none,
                   hintText: 'Enter Password',
                   fillColor: Color(0xfff3f3f4),
@@ -151,6 +197,38 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
+  // Widget _confirmPasswordField(String title, {bool isPassword = false}) {
+  //   return Container(
+  //     margin: EdgeInsets.symmetric(vertical: 10),
+  //     child: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: <Widget>[
+  //         Text(
+  //           title,
+  //           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+  //         ),
+  //         SizedBox(
+  //           height: 10,
+  //         ),
+  //         TextFormField(
+  //             controller: confirmPasswordController,
+  //             obscureText: isPassword,
+  //             keyboardType: TextInputType.name,
+  //             decoration: InputDecoration(
+  //                suffixIcon: confirmPasswordController.text.length > 0
+  //                     ? IconButton(
+  //                         onPressed: () => confirmPasswordController.clear(),
+  //                         icon: Icon(Icons.cancel, color: Colors.grey))
+  //                     : null,
+  //                 border: InputBorder.none,
+  //                 hintText: 'Confirm Password',
+  //                 fillColor: Color(0xfff3f3f4),
+  //                 filled: true))
+  //       ],
+  //     ),
+  //   );
+  // }
+
   Widget _submitButton(BuildContext context) {
     return GestureDetector(
       onTap: () async {
@@ -158,22 +236,26 @@ class _SignUpPageState extends State<SignUpPage> {
         form.save();
 
         if (form.validate()) {
-          try {
-            User result = await Provider.of<AuthService>(context, listen: false)
-                .createUser(
-                    username: usernameController.text,
-                    email: emailController.text,
-                    password: passwordController.text);
-            print(result);
-            //Add user info to Firebase
-            // addUserToDB();
-
-          } on FirebaseAuthException catch (error) {
-            return _buildErrorDialog(context, error.message);
-          } on Exception catch (error) {
-            return _buildErrorDialog(context, error.toString());
-          }
+          //if (passwordController.text == confirmPasswordController.text) {
+            submit = true;
+            try {
+              User result =
+                  await Provider.of<AuthService>(context, listen: false)
+                      .createUser(
+                          username: usernameController.text,
+                          email: emailController.text,
+                          password: passwordController.text);
+              print(result);
+            } on FirebaseAuthException catch (error) {
+              return _buildErrorDialog(context, error.message);
+            } on Exception catch (error) {
+              return _buildErrorDialog(context, error.toString());
+            }
+          //} else {
+            //_buildErrorDialog(context, "Passwords do not match");
+          //}
         }
+
         setState(() {
           Navigator.pushAndRemoveUntil(
             context,
